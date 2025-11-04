60a0e9ca962f8e470777b9606d12444efa7c86f3
aeternam-13 <mr.alfatlaloc@gmail.com>
2025-11-04 16:18:05 -0600

    version solving

--- app/src/main/java/com/aeternam/notesappmvi/feature_note/presentation/notes_screen/screen/NotesScreen.kt
+++ app/src/main/java/com/aeternam/notesappmvi/feature_note/presentation/notes_screen/screen/NotesScreen.kt
@@ -14,7 +14,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.aeternam.notesappmvi.feature_note.presentation.destinations.AddEditNoteDestination
